86a25854ea6143415d55bf845a419252079f6ece
alinarozhkovska <119886599+alinarozhkovska@users.noreply.github.com>
2023-02-24 22:22:41 +0200

    ДЗ 2. Калькулятор last changes

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ elif choice == '3':
     print(num1, "*", num2, "=", (num1*num2))
 
 elif choice == '4':
-    print(num1, "/", num2, "=", (num1//num2))
+    print(num1, "/", num2, "=", (num1/num2))
 
 else:
     print("Incorrect choice of operation")
